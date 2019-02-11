4a9d58fba11dfd22acabc77feb8363e799d8faa7
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-11 08:35:08 -0800

    Write pokemonIndexContainer

---
+++ frontend/components/pokemon/pokemon_index_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { selectAllPokemon } from '../../reducers/selectors';
+import { fetchAllPokemon } from '../../actions/pokemon_actions';
+import PokemonIndex from './pokemon_index';
+
+const mstp = state => {
+  return ({
+    pokemon: selectAllPokemon(state)
+  });
+};
+
+const mdtp = dispatch => {
+  return ({
+    fetchAllPokemon: () => fetchAllPokemon()
+  });
+};
+
+export default connect(mstp, mdtp)(PokemonIndex);
