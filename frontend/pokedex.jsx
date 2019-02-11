e3e2f1bdca2910497a7197760872cc473464e640
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-11 07:37:29 -0800

    Get frontend to load

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from ' react-dom';
+import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContendLoaded", () => {
-
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>Pokedex</h1>, rootEl)
 });
