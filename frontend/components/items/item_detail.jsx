5194ea8a9c0e9147c3c1a5e69076f9bf529dab85
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-12 07:44:35 -0800

    Fix detail bugs

--- frontend/components/items/item_detail.jsx
+++ frontend/components/items/item_detail.jsx
@@ -4,8 +4,8 @@ const ItemDetail = props => {
   return (
     <section>
       <h1>{props.item.name}</h1>
-      <h2>Price: {props.item.price}</h2>
       <h2>Happiness: {props.item.happiness}</h2>
+      <h2>Price: ${props.item.price}</h2>
     </section>
   );
 }
