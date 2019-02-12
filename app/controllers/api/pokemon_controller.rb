e51dc2be1da598da3178613e6823c7752c2aaf9d
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-12 07:52:32 -0800

    Write create action for pokemon

--- app/controllers/api/pokemon_controller.rb
+++ app/controllers/api/pokemon_controller.rb
@@ -6,4 +6,17 @@ class Api::PokemonController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
   end
+
+  def create
+    @pokemon = Pokemon.new(poke_params)
+    if @pokemon.save
+      render json: "/api/show"
+    else
+      render json: @pokemon.errors.full_messages, status: 400
+    end
+  end
+
+  def poke_params
+    params.require(:pokemon).permit(:name, :attack, :defense, :poke_type, :moves, :image_url)
+  end
 end
