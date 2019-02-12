a4021110f3f182877f7927c6b1a32190b137869d
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-12 09:40:23 -0800

    Set internal state for poke form

--- frontend/components/pokemon/pokemon_form.jsx
+++ frontend/components/pokemon/pokemon_form.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 class PokemonForm extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      name: '',
+      attack: '',
+      defense: '',
+      poke_type: '',
+      move1: '',
+      move2: '',
+      image_url: ''
+    };
+    
   }
 
   render(){
