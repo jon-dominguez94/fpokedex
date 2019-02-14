cbaa872df48c5051ebe411288e43df336e982b61
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-13 21:50:47 -0800

    Write pokemon erros reducer

---
+++ frontend/reducers/pokemon_errors_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_ALL_POKEMON, RECEIVE_POKEMON ,RECEIVE_POKEMON_ERRORS } from '../actions/pokemon_actions';
+import merge from 'lodash/merge';
+
+const _nullError = [];
+
+const PokemonErrorsReducer = (state = _nullError, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_POKEMON_ERRORS:
+      return merge({}, state, action.errors);
+    case RECEIVE_ALL_POKEMON:
+      return _nullError;
+    case RECEIVE_POKEMON:
+      return _nullError;
+    default:
+      return state;
+  }
+};
+
+export default PokemonErrorsReducer;
